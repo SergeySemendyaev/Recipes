4e960dc50ecfcb07bfb7b95596c440ad2d1c49c0
SSSemendyaev <14sam88@mail.ru>
2021-11-30 00:40:31 +0300

    Recipes final

---
+++ Recipes/task/src/recipes/RecipeService.java
@@ -0,0 +1,50 @@
+package recipes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class RecipeService {
+    final RecipyRepository recipyRepository;
+
+    Comparator<Recipe> dateComparator = (o1, o2) -> o2.getDate().compareTo(o1.getDate());
+
+    @Autowired
+    public RecipeService(RecipyRepository recipyRepository) {
+        this.recipyRepository = recipyRepository;
+    }
+
+    public Optional<Recipe> findById(long id) {
+        return recipyRepository.findById(id);
+    }
+
+    public Recipe save(Recipe recipe) {
+        return recipyRepository.save(recipe);
+   }
+
+    public void deleteById(long id) { recipyRepository.deleteById(id); }
+
+    public List<Recipe> findAllByCategory(String category) {
+        return findAll().stream()
+                .filter(e -> e.getCategory().equalsIgnoreCase(category))
+                .sorted(dateComparator)
+                .collect(Collectors.toList());
+    }
+
+    public List<Recipe> findAllByName(String name) {
+        return findAll().stream()
+                .filter(e -> e.getName().toLowerCase().contains(name.toLowerCase()))
+                .sorted(dateComparator)
+                .collect(Collectors.toList());
+    }
+
+    public List<Recipe> findAll() {
+        return (List<Recipe>) recipyRepository.findAll();
+    }
+}
+
